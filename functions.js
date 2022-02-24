1110b73018bf66999656264b943cc70b8a179f92
dparanou <dimitra.pra@gmail.com>
2022-02-24 13:40:04 +0200

    init commit

---
+++ functions.js
@@ -0,0 +1,32 @@
+// Get the elements with class="column"
+let elements = document.getElementsByClassName("column-images");
+
+// Declare a "loop" variable
+let i;
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "45%";
+  }
+  changeBtnsStyle();
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "15%";
+  }
+  changeBtnsStyle();
+}
+
+function changeBtnsStyle() {
+  let btns = document.getElementsByClassName("btn");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+}
